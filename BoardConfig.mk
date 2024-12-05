f20538f4ea1c2d7a8917119c647f1fcaf1760fbf
Arian <arian.kulmer@web.de>
2024-12-05 18:55:59 +0100

    mayfly: Commonize kernel module load list
    Change-Id: I13e6b6c9ab106d91b10d5e25dd92d86698da6bb0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,15 +19,6 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 # Display
 TARGET_SCREEN_DENSITY := 440
 
-# Kernel modules
-BOOT_KERNEL_MODULES += \
-    goodix_fod.ko \
-    qcom_pm8008-regulator.ko \
-    fts_touch_spi.ko \
-    qcom-hv-haptics.ko
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
-BOARD_VENDOR_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
-
 # Vibrator
 TARGET_QTI_VIBRATOR_EFFECT_LIB := libqtivibratoreffect.xiaomi
 TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM := true
