99ed25f255aa015a70bde32c49cd0d3aef39bd79
Arian <arian.kulmer@web.de>
2025-04-15 17:48:58 +0200

    mayfly: Update blobs and firmware from OS2.0.5.0.VLTCNXM
    Change-Id: If460cff74387e15e5778a6871a138bef8d0131d0

--- lineage_mayfly.mk
+++ lineage_mayfly.mk
@@ -21,8 +21,8 @@ PRODUCT_MODEL := 2206123SC
 PRODUCT_NAME := lineage_mayfly
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="mayfly-user 15 AQ3A.241006.001 OS2.0.4.0.VLTCNXM release-keys" \
-    BuildFingerprint=Xiaomi/mayfly/mayfly:15/AQ3A.241006.001/OS2.0.4.0.VLTCNXM:user/release-keys \
+    BuildDesc="mayfly-user 15 AQ3A.241006.001 OS2.0.5.0.VLTCNXM release-keys" \
+    BuildFingerprint=Xiaomi/mayfly/mayfly:15/AQ3A.241006.001/OS2.0.5.0.VLTCNXM:user/release-keys \
     DeviceProduct=mayfly \
     SystemName=mayfly
 
